44cb68b337163f55cb8a3a962b8bf96682318836
Victor <victor.augustosilva004@gmail.com>
2026-09-21 09:17:57 -0300

    first section

---
+++ section1/springsecuritysection1/src/test/java/com/eazybytes/springsecuritysection1/Springsecuritysection1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecuritysection1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecuritysection1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
